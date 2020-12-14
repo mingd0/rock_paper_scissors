d647ed35aff47485ab675e86876465f797e7b9c5
David Minge <minge.david@gmail.com>
2020-12-14 12:18:15 -0900

    Style fixes.

--- rps.py
+++ rps.py
@@ -1,89 +1,114 @@
 import random
 
-class Game: 
+
+class Game:
 
     CHOICES = ['r', 'p', 's']
 
-    def __init__(self): 
+    def __init__(self):
         self.scores = {
-            'player' : 0,
-            'computer' : 0,
+            'player': 0,
+            'computer': 0,
         }
-    
-    def check_player_validity(self, player): 
-        if player != 'player' and player != 'computer': 
+
+    def check_player_validity(self, player):
+        if player != 'player' and player != 'computer':
             raise ValueError("Argument 'player' is not valid")
 
     def get_score(self, player):
         self.check_player_validity(player)
         return self.scores[player]
 
-    def increment_score(self, player): 
+    def increment_score(self, player):
         self.check_player_validity(player)
         self.scores[player] += 1
 
-    def validatate_player_input(self, player_input): 
-        if not (player_input == 'r' or  player_input == 'p' or player_input == 's' or player_input == 'q'):
+    def validatate_player_input(self, player_input):
+        if not (player_input == 'r'
+                or player_input == 'p'
+                or player_input == 's'
+                or player_input == 'q'):
             return False
-        else: return True
-        
+        else:
+            return True
+
     def print_round_result(self, winner, player_score, computer_score):
-        if winner == 'player': 
+        if winner == 'player':
             message = "You win!"
-        elif winner == 'computer': 
-            message = "Computer wins!" 
-        elif winner == 'tie': 
+        elif winner == 'computer':
+            message = "Computer wins!"
+        elif winner == 'tie':
             message = "Tie!"
-        else: 
+        else:
             raise ValueError("Argument 'winner' is not valid")
 
-        if player_score < 0 or computer_score < 0: 
+        if player_score < 0 or computer_score < 0:
             raise ValueError("Player and computer score must be >= 0")
-        
-        print("%s Score: Player = %s, Computer = %s\n" % (message, player_score, computer_score))
 
-    def end_game(self): 
-        print("Game ended. Final score: Player = %s, Computer = %s" % (self.get_score('player'), self.get_score('computer')))
+        print("%s Score: Player = %s, Computer = %s\n" % (message,
+                                                          player_score,
+                                                          computer_score))
+
+    def end_game(self):
+        print("Game ended. Final score: Player = %s, Computer = %s"
+              % (self.get_score('player'), self.get_score('computer')))
         quit()
 
-    def game_logic(self, player_input, computer_input): # assume player_input has already been checked
+    def game_logic(self, player_input, computer_input):
         player_choice = player_input
         computer_choice = computer_input
-        if player_choice == 'q': 
+        if player_choice == 'q':
             return -999
-        if ((player_choice == 'r' and computer_choice == 's') 
-                or (player_choice == 's' and computer_choice == 'p') 
+        if ((player_choice == 'r' and computer_choice == 's')
+                or (player_choice == 's' and computer_choice == 'p')
                 or (player_choice == 'p' and computer_choice == 'r')):
             return 1
-        elif player_choice == computer_choice: 
+        elif player_choice == computer_choice:
             return 0
-        else: 
+        else:
             return -1
 
-    def game_controller(self, result): 
-        if result == 1: # Win
+    def game_controller(self, result):
+        """
+        Controls
+        """
+        if result == 1:
+            # Player wins
             self.increment_score('player')
-            self.print_round_result('player', self.get_score('player'), self.get_score('computer'))
-        elif result == 0: # Tie
-            self.print_round_result('tie', self.get_score('player'), self.get_score('computer'))
-        elif result == -1: 
+            self.print_round_result('player',
+                                    self.get_score('player'),
+                                    self.get_score('computer'))
+        elif result == 0:
+            # Tie
+            self.print_round_result('tie',
+                                    self.get_score('player'),
+                                    self.get_score('computer'))
+        elif result == -1:
+            # Player loses
             self.increment_score('computer')
-            self.print_round_result('computer', self.get_score('player'), self.get_score('computer'))
-        elif result == -999: 
+            self.print_round_result('computer',
+                                    self.get_score('player'),
+                                    self.get_score('computer'))
+        elif result == -999:
+            # Quit
             self.end_game()
 
-    def play(self): 
-        while True: # continue until game is ended
-            player_choice = input("Please enter your choice ('r/p/s'). Enter 'q' to quit: ")
+    def play(self):
+        while True:
+            player_choice = input("Please enter your choice ('r/p/s'). "
+                                  "Enter 'q' to quit: ")
             computer_choice = random.choice(self.CHOICES)
-            if self.validatate_player_input(player_choice): 
-                print("You chose %s, the computer chose %s." % (player_choice, computer_choice))
+            if self.validatate_player_input(player_choice):
+                print("You chose %s, the computer chose %s."
+                      % (player_choice, computer_choice))
+                # Determine who won the round
                 result = self.game_logic(player_choice, computer_choice)
+                # Print result and increment score appropriately
                 self.game_controller(result)
-            else: 
+            else:
                 print("Invalid entry, please enter 'r', 'p', 's', or 'q'.\n")
-  
-if __name__ == '__main__': 
+
+
+if __name__ == '__main__':
     gm = Game()
     gm.play()
-    
--- test.py
+++ test.py
@@ -4,48 +4,52 @@ from unittest.mock import patch
 
 from rps import Game
 
+
 class TestGame(unittest.TestCase):
 
-    def test_print_round_result_player(self): 
+    def test_print_round_result_player(self):
         expected_output = "You win! Score: Player = 1, Computer = 0\n\n"
-        with patch('sys.stdout', new = StringIO()) as fake_out: 
+        with patch('sys.stdout', new=StringIO()) as fake_out:
             Game.print_round_result(self, 'player', 1, 0)
-            self.assertEqual(fake_out.getvalue(), expected_output) 
+            self.assertEqual(fake_out.getvalue(), expected_output)
 
-    def test_print_round_result_computer(self): 
+    def test_print_round_result_computer(self):
         expected_output = "Computer wins! Score: Player = 0, Computer = 1\n\n"
-        with patch('sys.stdout', new = StringIO()) as fake_out: 
+        with patch('sys.stdout', new=StringIO()) as fake_out:
             Game.print_round_result(self, 'computer', 0, 1)
-            self.assertEqual(fake_out.getvalue(), expected_output) 
+            self.assertEqual(fake_out.getvalue(), expected_output)
 
-    def test_print_round_result_tie(self): 
+    def test_print_round_result_tie(self):
         expected_output = "Tie! Score: Player = 0, Computer = 0\n\n"
-        with patch('sys.stdout', new = StringIO()) as fake_out: 
+        with patch('sys.stdout', new=StringIO()) as fake_out:
             Game.print_round_result(self, 'tie', 0, 0)
-            self.assertEqual(fake_out.getvalue(), expected_output) 
+            self.assertEqual(fake_out.getvalue(), expected_output)
 
     def test_print_round_result_invalid_score(self):
-        self.assertRaises(ValueError, Game.print_round_result, self, 'computer', 0, -1)
+        self.assertRaises(ValueError,
+                          Game.print_round_result, self, 'computer', 0, -1)
 
-    def test_print_round_result_invalid_player(self): 
-        self.assertRaises(ValueError, Game.print_round_result, self, 'invalid_entry', 0, 1)
+    def test_print_round_result_invalid_player(self):
+        self.assertRaises(ValueError,
+                          Game.print_round_result, self, 'invalid_entry', 0, 1)
 
     def test_check_player_validity(self):
-        self.assertRaises(ValueError, Game.check_player_validity, self, 'invalid_entry')
+        self.assertRaises(ValueError,
+                          Game.check_player_validity, self, 'invalid_entry')
 
-    def test_get_score_player(self): 
+    def test_get_score_player(self):
         gm = Game()
         self.assertEqual(gm.get_score('player'), 0)
 
-    def test_get_score_computer(self): 
+    def test_get_score_computer(self):
         gm = Game()
         self.assertEqual(gm.get_score('computer'), 0)
 
-    def test_get_score_invalid(self): 
+    def test_get_score_invalid(self):
         gm = Game()
-        self.assertRaises(ValueError, gm.get_score, 'invalid_entry') 
+        self.assertRaises(ValueError, gm.get_score, 'invalid_entry')
 
-    def test_increment_score(self): 
+    def test_increment_score(self):
         gm = Game()
         gm.increment_score('player')
         self.assertEqual(gm.get_score('player'), 1)
@@ -57,13 +61,15 @@ class TestGame(unittest.TestCase):
         self.assertTrue(Game.validatate_player_input(self, 'q'))
         self.assertFalse(Game.validatate_player_input(self, 'invalid_input'))
 
-    def test_game_logic(self): 
+    def test_game_logic(self):
         choices = ['r', 'p', 's']
         result = []
         expected_result = [0, -1, 1, 1, 0, -1, -1, 1, 0]
-        for player_choice in choices: 
-            for computer_choice in choices: 
-                result.append(Game.game_logic(self, player_choice, computer_choice))
+        for player_choice in choices:
+            for computer_choice in choices:
+                result.append(Game.game_logic(self,
+                                              player_choice,
+                                              computer_choice))
         self.assertEqual(result, expected_result)
         self.assertEqual(Game.game_logic(self, 'q', 's'), -999)
 
@@ -76,5 +82,6 @@ class TestGame(unittest.TestCase):
         self.assertEqual(gm.get_score('player'), 2)
         self.assertEqual(gm.get_score('computer'), 1)
 
-if __name__ == '__main__': 
+
+if __name__ == '__main__':
     unittest.main()
